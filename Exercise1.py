1966d5d833c64d8a6504cbb038ed74c77b24ac16
filifil <filifilibro@gmail.com>
2024-05-18 14:54:16 +0200

    practice exam

--- Exercise1.py
+++ Exercise1.py
@@ -1,5 +1,8 @@
 num1 = [1,2,'moon',3,'fruit']
 num2 = [4,5,'sun',6,'appel']
-new_num = dict(zip(num1,num2))
-     
+new_num = dict()
+for i in range(len(num1)):
+    new_num.update({num1[i]:num2[i]})
 print(new_num)
+     
+
